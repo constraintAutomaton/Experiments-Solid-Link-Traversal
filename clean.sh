a3091b5c0cf24aa709b27243e6e44e5229646276
Bryan-Elliott Tam <bryanelliott.tam@ugent.be>
2024-03-08 11:58:19 +0100

    do not delete the lock file and index and deleted experiment deleted from script files

--- clean.sh
+++ clean.sh
@@ -3,29 +3,19 @@ rm -rf node_modules
 
 pushd experiments
 
-pushd index-vs-storage
-rm -f yarn.lock
-rm -rf node_modules
-rm -r generated/*
-rm -r combinations/*
-popd
-
 pushd queries-short
-rm -f yarn.lock
 rm -rf node_modules
 rm -r generated/*
 rm -r combinations/*
 popd
 
 pushd queries-discover
-rm -f yarn.lock
 rm -rf node_modules
 rm -r generated/*
 rm -r combinations/*
 popd
 
 pushd queries-complex
-rm -f yarn.lock
 rm -rf node_modules
 rm -r generated/*
 rm -r combinations/*
