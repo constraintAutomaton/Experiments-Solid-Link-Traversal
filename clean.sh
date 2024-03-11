bfcd108c3d1b161d553f9c2dcc850b2051dd84e1
Bryan-Elliott Tam <bryanelliott.tam@ugent.be>
2024-03-11 09:34:45 +0100

    combination working

--- clean.sh
+++ clean.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 rm -rf node_modules
 
+yarn unlink "rdf-dataset-fragmenter"
+yarn unlink "query-shape-detection"
+yarn unlink "@comunica/config-query-sparql-link-traversal"
+yarn unlink "@comunica/actor-context-preprocess-key-filter"
+yarn unlink "@comunica/actor-extract-links-shape-index"
+yarn unlink "@comunica/mediator-combine-array"
+yarn unlink "@comunica/actor-rdf-resolve-hypermedia-links-queue-wrapper-filter-links"
+
 pushd experiments
 
 pushd queries-short
--- generate-combinations.sh
+++ generate-combinations.sh
@@ -2,18 +2,22 @@
 pushd experiments
 
 pushd queries-short
+cp -rf ../../config-client input/config-client
 yarn jbr generate-combinations
 popd
 
 pushd queries-discover
+cp -rf ../../config-client input/config-client
 yarn jbr generate-combinations
 popd
 
 pushd queries-complex
+cp -rf ../../config-client input/config-client
 yarn jbr generate-combinations
 popd
 
 pushd fragmentation
+cp -rf ../../config-client input/config-client
 yarn jbr generate-combinations
 popd
 
