2b7337e37b40a4fb5182bf001cfd3cb54054b29d
Bryan-Elliott Tam <bryanelliott.tam@ugent.be>
2024-03-15 10:25:54 +0100

    change the building procedure of rdf-dataset-fragmenter

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@ yarn install --ignore-engines
 
 pushd rdf-dataset-fragmenter.js
     # might return an error but it is still compiling
+    yarn build
     yarn build:components
-    yarn build:ts
 popd
