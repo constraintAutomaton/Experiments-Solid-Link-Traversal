b70f8c44009ac871832f7a4f12a6fefe8983d222
Bryan-Elliott Tam <bryanelliott.tam@ugent.be>
2024-03-15 14:40:20 +0100

    do not remove generated

--- clean.sh
+++ clean.sh
@@ -5,28 +5,24 @@ pushd experiments
 
     pushd queries-short
         rm -rf node_modules
-        rm -r generated/*
         rm -r combinations/*
         rm -r output/*
     popd
 
     pushd queries-discover
         rm -rf node_modules
-        rm -r generated/*
         rm -r combinations/*
         rm -r output/*
     popd
 
     pushd queries-complex
         rm -rf node_modules
-        rm -r generated/*
         rm -r combinations/*
         rm -r output/*
     popd
 
     pushd fragmentation
         rm -rf node_modules
-        rm -r generated/*
         rm -r combinations/*
         rm -r output/*
     popd
