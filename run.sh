e51bb22620ecb2b6e92407d7871a6f23710c3c66
Bryan-Elliott Tam <bryanelliott.tam@ugent.be>
2024-03-15 14:42:01 +0100

    run procedure modified

--- run.sh
+++ run.sh
@@ -4,17 +4,23 @@ pushd experiments
     pushd queries-short
         ./run.sh
     popd
+    ../backup_procedure/backup.sh
 
     pushd queries-discover
         ./run.sh
     popd
+    ../backup_procedure/backup.sh
 
     pushd queries-complex
         ./run.sh
     popd
+    ../backup_procedure/backup.sh
 
     pushd fragmentation
         ./run.sh
     popd
-
+    ../backup_procedure/backup.sh
+    
 popd
+
+./backup_procedure/backup.sh
