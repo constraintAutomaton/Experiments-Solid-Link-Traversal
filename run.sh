ec3eada25a36f016815db3294d8a6c45cedab823
Bryan-Elliott Tam <bryanelliott.tam@ugent.be>
2024-03-18 11:57:13 +0100

    fix the backup problem

--- run.sh
+++ run.sh
@@ -4,23 +4,27 @@ pushd experiments
     pushd queries-short
         ./run.sh || true
     popd
-    ../backup_procedure/backup.sh || true
+    cd ./../backup_procedure && ./backup.sh || true
+    cd ./../experiments
 
     pushd queries-discover
         ./run.sh || true
     popd
-    ../backup_procedure/backup.sh || true
+    cd ./../backup_procedure && ./backup.sh || true
+    cd ./../experiments
 
     pushd queries-complex
         ./run.sh || true
     popd
-    ../backup_procedure/backup.sh || true
+    cd ./../backup_procedure && ./backup.sh || true
+    cd ./../experiments
 
     pushd fragmentation
         ./run.sh || true
     popd
-    ../backup_procedure/backup.sh || true
+    cd ./../backup_procedure && ./backup.sh || true
+    cd ./../experiments
     
 popd
 
-./backup_procedure/backup.sh || true
+cd ./backup_procedure && ./backup.sh || true
