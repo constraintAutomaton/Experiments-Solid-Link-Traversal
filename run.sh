9a880a67c49250ecdecbea0e9ba9b421076410ed
Bryan-Elliott Tam <bryanelliott.tam@ugent.be>
2024-03-19 15:25:16 +0100

    fix the path of the errors

--- run.sh
+++ run.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
-(cd ./backup_procedure && ./backup.sh) 2> ./../../errors
+(cd ./backup_procedure && ./backup.sh) 2>> ./../errors
 
 pushd experiments
 
     pushd queries-short
-        ./run.sh 2> ./../../errors
+        ./run.sh 2>> ./../../errors
     popd
 
-    (cd ./../backup_procedure && ./backup.sh) 2> ./../../errors
+    (cd ./../backup_procedure && ./backup.sh) 2>> ./../../errors
     cd ./../experiments
 
     pushd queries-discover
-        ./run.sh 2> ./../../errors
+        ./run.sh 2>> ./../../errors
     popd
-    (cd ./../backup_procedure && ./backup.sh) 2> ./../../errors
+    (cd ./../backup_procedure && ./backup.sh) 2>> ./../../errors
     cd ./../experiments
 
     pushd queries-complex
-        ./run.sh 2> ./../../errors
+        ./run.sh 2>> ./../../errors
     popd
-    (cd ./../backup_procedure && ./backup.sh) 2> ./../../errors
+    (cd ./../backup_procedure && ./backup.sh) 2>> ./../../errors
     cd ./../experiments
 popd
 
-cd ./backup_procedure && ./backup.sh 2> ./../../errors
+cd ./backup_procedure && ./backup.sh 2>> ./../errors
